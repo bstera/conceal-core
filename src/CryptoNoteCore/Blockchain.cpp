c165260cdfa4f7e29ece78611423b20e3247142c
katz <cryptokatz1@protonmail.com>
2020-11-13 14:02:45 +0530

    Set manual difficulty

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -882,6 +882,11 @@ namespace CryptoNote
     uint64_t block_index = m_blocks.size();
     uint8_t block_major_version = get_block_major_version_for_height(block_index + 1);
 
+    if ((block_index > 636040) && (block_index < 636100)) {
+      return 200;
+    }
+
+
     if (block_major_version >= 8)
     {
       return m_currency.nextDifficultyLWMA1(timestamps, commulative_difficulties, block_index);
