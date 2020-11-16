f38152bcb0d44aea222367856377b3bf5cada601
katz <cryptokatz1@protonmail.com>
2020-11-16 12:37:50 +0530

    Update Currency.cpp

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -191,7 +191,7 @@ namespace CryptoNote
 
     if (height > CryptoNote::parameters::UPGRADE_HEIGHT_V8)
     {
-      base_reward = CryptoNote::parameters::MAX_BLOCK_REWARD_V1;
+      base_reward = CryptoNote::MAX_BLOCK_REWARD_V1;
     }
     else
     {
